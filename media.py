3677b23bf2fa4c9681e2c6afb73c8201d44403df
PavaniVegi <33685787+PavaniVegi@users.noreply.github.com>
2017-11-16 12:18:25 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,13 +1,18 @@
 import webbrowser
 
-#define movie class and it properties
-class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_img, trailer_youtuber_url):
+
+# define movie class and it properties
+class Movie():  
+    """ Movie class consists the structure of object """
+    def __init__(self, movie_title, movie_storyline, 
+                 poster_img, trailer_youtuber_url):
+        """constructer of movie class defines all the key variables"""
+        """in Movie class"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_img
         self.trailer_youtube_url = trailer_youtuber_url
-        
+
     def show_trailer(self):
-        #open url in web browser
+        """on call of show_trailer method open's url in web browser"""
         webbrowser.open(self.trailer_youtube_url)
